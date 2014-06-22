dc8bbf556fe9f9ce668fe644cc8642db77fc13e2
Igor Sarcevic <igisar@gmail.com>
2014-06-22 14:07:40 +0200

    Bootstrap the project.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+RSpec.configure do |config|
+  
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 10
+  config.order = :random
+  
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+    mocks.syntax = :expect
+
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended.
+    mocks.verify_partial_doubles = true
+  end
+end
