2948f03fc7ce5f2a430b29f98569faaf98b38e40
Igor Sarcevic <igisar@gmail.com>
2014-07-03 09:40:20 +0200

    Environments start.

--- src/evaluator.rb
+++ src/evaluator.rb
@@ -4,27 +4,41 @@ class Evaluator
   end
 
   def run
-    evaluate(@source)
+    evaluate(@source, [])
   end
 
   private
 
-  def evaluate(input)
+  def evaluate(input, env_stack)
+    env_stack << {}
+
     if input[:type] == :structure
 
-      head = evaluate input[:value][0]
-      tail = input[:value][1..-1].map { |el| evaluate el }
+      #head = evaluate input[:value][0], env_stack
+      head = input[:value][0][:value]
 
-      case head
-      when "+" then sum tail
-      when "*" then product tail
-      when "exit" then exit(1)
-      else raise "unrecognized symbol"
+      if head == "let"
+        return define(input[:value][1..-1], env_stack)
       end
 
+      tail = input[:value][1..-1].map { |el| evaluate el, env_stack }
+
+      return case head
+        when "+" then sum tail
+        when "*" then product tail
+        when "exit" then exit(1)
+        else raise "unrecognized symbol"
+        end
+
     else
-      input[:value]
+      if input[:type] == :atom
+        return lookup(input[:value], env_stack)
+      else
+        return input[:value]
+      end
     end
+
+    env_stack.pop
   end
 
   def sum(elements)
@@ -37,5 +51,13 @@ class Evaluator
     sum = 1
     elements.each { |el| sum *= el }
     sum
+  end
+
+  def define(elements, env_stack)
+    env_stack.last[elements[0]] = elements[1]
+  end
+
+  def lookup(element, env_stack)
+    env_stack[element]
   end
 end
